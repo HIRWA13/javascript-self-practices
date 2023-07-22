0ee5f09f96c82811bab4cb552bedfdb3df454be8
HIRWA13 <iamhirwejr@gmail.com>
2023-07-22 21:52:50 +0200

    a function that can help you to print the contents of the current window

---
+++ Basic/printContentsOfCurrentWindow.js
@@ -0,0 +1,7 @@
+// 1. below function can be used to print the contents of the current Window.
+const print = document.getElementById('print');
+function printCurrentWindow() {
+    window.print();
+}
+print.addEventListener('click', printCurrentWindow);
+
