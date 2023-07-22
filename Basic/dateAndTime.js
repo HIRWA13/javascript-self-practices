1e3bb80ea3ea63e4149cc8da1be1f2fbb678f933
HIRWA13 <iamhirwejr@gmail.com>
2023-07-22 21:29:08 +0200

    date and time exercise

---
+++ Basic/dateAndTime.js
@@ -0,0 +1,23 @@
+ /** 
+Sample Output : Today is : Tuesday.
+Current time is : 10 PM : 30 : 38
+  */
+ // 1. Create a new date object
+ // 2. Get the day of the week
+// 3. Get the current time
+
+//1. Create a new date object
+let date = new Date();
+//2. Get the day of the week
+let day = date.getDay();   
+//3. Get the current time
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+//4. Create an array of days of the week
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//5. Print the day of the week
+console.log('Today is: ' + days[day]);
+//6. Print the current time
+console.log('Current time is: ' + hours + ' ' + ampm + ' : ' + minutes + ' : ' + seconds);
